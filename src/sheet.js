a44d4c08f9fb511293f4c45078c3b37069bd7333
itzmeowww <winthanasan@gmail.com>
2021-06-07 08:57:41 +0700

    fix space error

--- src/sheet.js
+++ src/sheet.js
@@ -113,9 +113,8 @@ async function gsrun(cl) {
       });
       if (data.data.values) {
         data.data.values.forEach((x) => {
-          if (x.length != 0 && x[0] != "" && x[0] != date) {
+          if (x.length != 0 && x[0] != "" && x[0] != " " && x[0] != date) {
             date = moment(x[0], "DD/MM/YYYY").format("M/D/YYYY");
-            // if (date != "Invalid date") console.log(date);
           }
           if (date != "" && date != "Invalid date") {
             //   console.log(database);
